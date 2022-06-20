5a7fc1bdb351efd327d29eb851b3844b2e063b3e
SleepyTrap <pascalraber@gmx.de>
2022-06-20 23:29:06 +0200

    sleep_e v1

--- keyboards/sleep_e/keymaps/default/keymap.c
+++ keyboards/sleep_e/keymaps/default/keymap.c
@@ -26,10 +26,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_BSPC, \
         KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOTE, KC_ENT,  \
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_RSFT, \
-
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH, KC_RSFT, \
         KC_LCTL, KC_LALT, MO(_FN1), MO(_FN2), KC_SPC, KC_RALT, KC_RGUI, KC_RCTL
     ),
-    [_FN1] = LAYOUT(),
-    [_FN2] = LAYOUT()
+    [_FN1] = LAYOUT(
+      KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_BSPC, \
+      KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOTE, KC_ENT,  \
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH, KC_RSFT, \
+      KC_LCTL, KC_LALT, MO(_FN1), MO(_FN2), KC_SPC, KC_RALT, KC_RGUI, KC_RCTL
+    ),
+    [_FN2] = LAYOUT(
+      KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_BSPC, \
+      KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOTE, KC_ENT,  \
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH, KC_RSFT, \
+      KC_LCTL, KC_LALT, MO(_FN1), MO(_FN2), KC_SPC, KC_RALT, KC_RGUI, KC_RCTL
+    )
 };
--- keyboards/sleep_e/sleep_e.c
+++ keyboards/sleep_e/sleep_e.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "3dwf.h"
+#include "sleep_e.h"
 
 // Optional override functions below.
 // You can leave any or all of these undefined.
