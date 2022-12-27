0449501affbc00522361bb790c627ef2aa03cea3
SleepyTrap <pascalraber@gmx.de>
2022-12-27 20:04:27 +0100

    compiled and works now

--- keyboards/sleep_e/config.h
+++ keyboards/sleep_e/config.h
@@ -42,8 +42,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define DEBOUNCE 5
-
-
-/* disable these deprecated features by default */
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
